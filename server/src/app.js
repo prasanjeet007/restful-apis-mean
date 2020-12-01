fae4589def61aab58d184c9eb0b0492dd8838fd4
prasanjeet chatterjee <prasanjeetchatterjee28@gmail.com>
2020-12-01 09:40:17 +0530

    new-cors-fixed

--- server/src/app.js
+++ server/src/app.js
@@ -5,8 +5,15 @@ require('../mongoose/mongoose');
 const routerr = require('../routers/router');
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(cors());
-app.options('*', cors());
+app.use(function( req, res, next ) {
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Headers", "x-requested-with, content-type");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    // res.header("Access-Control-Max-Age", "1000000000");
+    next();
+});
+    // intercept OPTIONS method if ('OPTIONS' == req.method) { res.send(200); } else { next(); } });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(express.json());
