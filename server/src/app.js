f9fd1c21b45f87aa4afc7416809d7e01ff225039
prasanjeet chatterjee <prasanjeetchatterjee28@gmail.com>
2020-12-01 10:17:26 +0530

    cors error fix

--- server/src/app.js
+++ server/src/app.js
@@ -7,15 +7,16 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Credentials", '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    res.header("Access-Control-Allow-Headers", '*');
     next();
 });
+// app.use(cors());
     // intercept OPTIONS method if ('OPTIONS' == req.method) { res.send(200); } else { next(); } });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(express.json());
+// app.use(express.json())
 app.use(routerr);
 app.get('/', (req, res) => {
     res.send('Hello from server');
