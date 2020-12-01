fc97452f9bb01b5d3d1b923722de82e443b814b4
prasanjeet chatterjee <prasanjeetchatterjee28@gmail.com>
2020-12-01 10:03:11 +0530

    cors enable

--- server/src/app.js
+++ server/src/app.js
@@ -5,14 +5,11 @@ require('../mongoose/mongoose');
 const routerr = require('../routers/router');
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(function( req, res, next ) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header('Access-Control-Allow-Headers: Origin, Content-Type, application/json');
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-    // res.header("Access-Control-Allow-Credentials", "true");
-    // res.header("Access-Control-Max-Age", "1000000000");
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-});
+  });
     // intercept OPTIONS method if ('OPTIONS' == req.method) { res.send(200); } else { next(); } });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
