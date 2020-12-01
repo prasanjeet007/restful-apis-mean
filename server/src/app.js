9d05dfcbb9a8f531563e88b0d6d6f2642d7e9777
prasanjeet chatterjee <prasanjeetchatterjee28@gmail.com>
2020-12-01 10:31:28 +0530

    cors bg fixed

--- server/src/app.js
+++ server/src/app.js
@@ -12,7 +12,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", '*');
     next();
 });
-// app.use(cors());
+app.use(cors());
     // intercept OPTIONS method if ('OPTIONS' == req.method) { res.send(200); } else { next(); } });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
