272c57bc84d9ed953eae3529cb43548466a3bca9
prasanjeet chatterjee <prasanjeetchatterjee28@gmail.com>
2020-12-01 09:34:20 +0530

    cors bug reolved

--- server/src/app.js
+++ server/src/app.js
@@ -3,26 +3,10 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 require('../mongoose/mongoose');
 const routerr = require('../routers/router');
-// add this code
-const whitelist = ['http://localhost:4200', 'https://intense-island-95023.herokuapp.com']; // list of allow domain
-
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin) {
-            return callback(null, true);
-        }
-
-        if (whitelist.indexOf(origin) === -1) {
-            var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
-}
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(cors(corsOptions));
+app.use(cors());
+app.options('*', cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(express.json());
