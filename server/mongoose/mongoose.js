b7722d9b54ce231abd2084c2f9787ef18e206519
prasanjeet chatterjee <prasanjeetchatterjee28@gmail.com>
2020-12-03 09:49:50 +0530

    mongoose update

--- server/mongoose/mongoose.js
+++ server/mongoose/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // const url = process.env.MONGO_URL;
-mongoose.connect('mongodb+srv://admin:<password>@cluster0.wpeu1.mongodb.net/<dbname>?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true}).then(data => {
+mongoose.connect('mongodb+srv://admin:<password>@cluster0.wpeu1.mongodb.net/mean-stack?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true}).then(data => {
     if (data) {
         console.log('Connection is successful');
     }
